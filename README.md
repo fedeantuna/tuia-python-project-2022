7c969f9e1732c495c92f4e8eb1211cfcf8c56bd6
Federico Antuña <federicoantuna@hotmail.com>
2022-06-29 10:39:13 -0300

    Added set up instructions

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-## Data structure to read (JSON):
+# Set Up
+
+With Python 3.x installed, these are the steps to follow to set up the project.
+
+## Virtual Environment
+
+In order to avoid issues with the dependencies, we set up the virtual environment. To do that run the following command from your terminal while on the root of the project:
+
+`python -m venv venv`
+
+Alternatively, if you have multiple versions of python installed is probably that `python` points to something like Python 2.7 and `python3` points to Python 3.x. In that case run:
+
+`python3 -m venv venv`
+
+Once the virtual environment is set up, activate it by running:
+
+`source venv/bin/activate` (on Unix-like systems)
+
+`.\venv\bin\Activate.ps1` (on Windows)
+
+If successful, you should see a `(venv)` at the beginning of the prompt on your terminal.
+
+After that, upgrade the `pip` version on that virtual environment by running:
+
+`pip install --upgrade pip`
+
+## Install dependencies
+
+The dependencies needed for this project are listed in `requirements.txt`, to install them make sure that you are on the virtual environment and simply run:
+
+`pip install -r requirements.txt`
+
+## Run
+
+While in the virtual environment, run `python main.py` or `python3 main.py` depending on your system.
+
+# Data structure to read (JSON):
 ```
 {
     "neighbourhood 1": {
@@ -54,7 +90,7 @@
 }
 ```
 
-## Data structure procesed (JSON):
+# Data structure procesed (JSON):
 ```
 {
     "neighbourhood 1": {
--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 from airbnb_calendar import get_calendar_data
-from functions import average
 from airbnb_listings import get_listings_data
+from functions import average
 
-calendar_data = get_calendar_data()
-listings_data = get_listings_data(calendar_data)
+def main():
+    airbnb_data = _read_airbnb_data()
+    
+    data = {}
 
-data = {}
+    for neighbourhood in airbnb_data:
+        data[neighbourhood] = {}
 
-for neighbourhood in listings_data:
-    data[neighbourhood] = {}
+        for room_type in airbnb_data[neighbourhood]:
+            data[neighbourhood][room_type] = {}
 
-    for room_type in listings_data[neighbourhood]:
-        data[neighbourhood][room_type] = {}
+            price_sum = airbnb_data[neighbourhood][room_type]['price_sum']
+            rating_sum = airbnb_data[neighbourhood][room_type]['rating_sum']
+            occupancy_percentage_sum = airbnb_data[neighbourhood][room_type]['occupancy_percentage_sum'] * 100
+            counter = airbnb_data[neighbourhood][room_type]['counter']
 
-        price_sum = listings_data[neighbourhood][room_type]['price_sum']
-        rating_sum = listings_data[neighbourhood][room_type]['rating_sum']
-        occupancy_percentage_sum = listings_data[neighbourhood][room_type]['occupancy_percentage_sum'] * 100
-        counter = listings_data[neighbourhood][room_type]['counter']
+            average_occupancy = average(occupancy_percentage_sum, counter)
+            average_price = average(price_sum, counter)
+            average_rating = average(rating_sum, counter)
 
-        average_occupancy = average(occupancy_percentage_sum, counter)
-        average_price = average(price_sum, counter)
-        average_rating = average(rating_sum, counter)
+            data[neighbourhood][room_type] = {
+                'average_occupancy': average_occupancy,
+                'average_price': average_price,
+                'average_rating': average_rating
+            }
 
-        data[neighbourhood][room_type] = {
-            'average_occupancy': average_occupancy,
-            'average_price': average_price,
-            'average_rating': average_rating
-        }
+    print(data)
 
-print(data)
+def _read_airbnb_data():
+    calendar_data = get_calendar_data()
+    listings_data = get_listings_data(calendar_data)
+
+    return listings_data
+
+main()
